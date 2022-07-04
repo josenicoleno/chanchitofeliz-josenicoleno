92686a6e9696661e3fa29dc967ee52257015412c
josenicoleno <josenicoleno@gmail.com>
2022-07-03 23:37:36 -0300

    entregable 4

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 const ItemListContainer = ({greeting}) => {
-    return(<div>{greeting}</div>
+    return(
+        <div>
+            {greeting}
+            <ItemCount stock={5} initial={1} onAdd={(n)=> alert(`${n} productos agregados`)}/>
+        </div>
     )
 }
 export default ItemListContainer;
