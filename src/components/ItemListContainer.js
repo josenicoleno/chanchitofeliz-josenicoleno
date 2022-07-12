820611d1a7c10608e90947e009f64c9f47514d05
josenicoleno <josenicoleno@gmail.com>
2022-07-11 21:05:55 -0300

    entregable 5 casi

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import ItemCount from "./ItemCount";
+import ItemList from "./ItemList";
+import products from "../products.json"
 
 const ItemListContainer = ({greeting}) => {
+    function onAddCallback(n){
+        alert(`${n} productos agregados`)
+    }
+   // console.log(products)
     return(
         <div>
             {greeting}
-            <ItemCount stock={5} initial={1} onAdd={(n)=> alert(`${n} productos agregados`)}/>
+            <ItemCount stock={5} initial={1} onAdd={onAddCallback}/>
+
+            <ItemList items={products}/>
         </div>
     )
 }
